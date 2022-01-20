256952dfd0ee0d692271a92f8a4bf0a2dc5ceecc
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-20 13:24:42 -0300

    Middleware QueryParser Int / Backend finished

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,16 @@ const port = 3005;
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const queryParser = require('express-query-int');
 const server = express();
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(queryParser());
+
+// to accept request of different origins
+server.use(allowCors);
 
 server.listen(port, function() {
   console.log(`Backend is running on port ${port}.`);
