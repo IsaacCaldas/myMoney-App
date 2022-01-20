f8e75cb6b867ad2cf42780eca8d5c499f41239a8
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-20 15:28:31 -0300

    Navigation with react-router

---
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default props => {
+
+  return (
+
+    <div>
+      <h1>Billing Cycle
+      </h1>
+    </div>
+
+  );
+
+}
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,6 @@
 import '../common/template/dependencies';
 import React from "react";
+import Routes from './routes';
 
 import Header from '../common/template/Header'
 import SideBar from '../common/template/SideBar';
@@ -13,7 +14,7 @@ export default props => {
       <Header />
       <SideBar />
       <div className="content-wrapper">
-        <h1>Main side content</h1>
+        <Routes />
       </div>
       <Footer />
     </div>
