c32701f5ec6ebd2b352ad38587cee1cf47f9ac81
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-21 14:02:00 -0300

    Showing success/error messages

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import { reducer as toastrReducer } from 'redux-reducer-toastr';
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducer from '../dashboard/DashboardReducer';
 import tabReducer from '../common/tab/tabReducer';
